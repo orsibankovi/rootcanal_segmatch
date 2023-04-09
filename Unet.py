0f181b0d528f4c03e9175f04e524f3cf25c6b03c
Orsolya Bánkövi <bankovi.orsolya@edu.bme.hu>
2023-04-09 16:47:24 +0200

    get dataset

--- Unet.py
+++ Unet.py
@@ -1,3 +1,5 @@
+import os
+
 import rootcanal_segmatch
 import torch
 import cv2
@@ -8,20 +10,22 @@ from torchvision import transforms, utils
 class GetDataset(Dataset):
     def __init__(self):
         super(GetDataset, self).__init__()
-        self.path_input = "/fogak/segmentation/original"
-        self.input_names = list(filter(lambda x: x.endswith(".png"), list(self.path_input)))
+        self.path_input = "C:/Users/banko/Desktop/BME_VIK/I_felev/onlab1/fogak/segmentation/original"
+        self.input = os.listdir(self.path_input)
+        self.input_names = list(filter(lambda x: x.endswith(".png"), list(self.input)))
         self.input_images = []
         for images in self.input_names:
             img_temp = cv2.imread(self.path_input + '/' + images, cv2.IMREAD_GRAYSCALE)
-            img_temp = torch.from_numpy(img_temp.reshape((1, 501, 501))).double()
+            img_temp = torch.from_numpy(img_temp.reshape((1, 501, 501))).int()
             self.input_images.append(img_temp)
 
-        self.path_target = "/fogak/segmentation/inverse"
-        self.target_names = list(filter(lambda x: x.endswith(".png"), list(self.path_target)))
+        self.path_target = "C:/Users/banko/Desktop/BME_VIK/I_felev/onlab1/fogak/segmentation/inverse"
+        self.target = os.listdir(self.path_target)
+        self.target_names = list(filter(lambda x: x.endswith(".png"), list(self.target)))
         self.target_images = []
         for images in self.target_names:
-            img_temp = cv2.imread(self.path_input + '/' + images, cv2.IMREAD_GRAYSCALE)
-            img_temp = torch.from_numpy(img_temp.reshape((1, 501, 501))).double()
+            img_temp = cv2.imread(self.path_target + '/' + images, cv2.IMREAD_GRAYSCALE)
+            img_temp = torch.from_numpy(img_temp.reshape((1, 501, 501))).int()
             self.target_images.append(img_temp)
 
     def __getitem__(self, index):
@@ -29,3 +33,11 @@ class GetDataset(Dataset):
 
     def __len__(self):
         return len(self.input_images)
+
+
+dataset = GetDataset()
+i = dataset.__len__()
+print(i)
+x, y = dataset.__getitem__(100)
+print(x)
+print(y)
